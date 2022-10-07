07f0e33b4ebc39e2f6ae5f828d64e77a6702db3a
sandhiya-sampath <sandhiya.sampath@exathought.com>
2022-10-07 12:23:32 +0530

    added test string .java file

---
+++ TestString.java
@@ -0,0 +1,17 @@
+public class TestString {
+    public static void main(String[] args) {
+        String s = new String(args[0]);
+      //  System.out.println(s);
+                //System.out.println(args[1]);
+        
+        String s1 = "kaviya";
+      int length = s.length();
+        //System.out.println(length);
+        
+        
+        int [] intArr=  {10,3,4,7,8,9,10};
+        System.out.println(intArr);
+        System.out.println(intArr[0]);
+        
+    }
+}
